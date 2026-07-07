d97a5f252a4485a81de3081a187452e572959d9d
Sambhav Narula <narulasambhav24@gmail.com>
2026-07-07 17:08:18 +0530

    Time: 2 ms (92.15%), Space: 47.6 MB (75.42%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,11 @@
 class Solution {
-    public void swap(int[] arr, int i, int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
     public void moveZeroes(int[] nums) {
+        int p = 0;
         for(int i = 0; i < nums.length; i++){
-            for(int j = i+1; j < nums.length; j++){
-                if(nums[i] == 0 && nums[j] != 0){
-                    swap(nums, i, j);
-                }
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[p];
+                nums[p++] = temp;
             }
         }
     }
