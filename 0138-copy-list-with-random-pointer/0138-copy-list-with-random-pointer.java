947c6729cb6c8b5eebb8cf27ba3917abcac8ac3c
Sambhav Narula <narulasambhav24@gmail.com>
2026-08-19 12:23:29 +0530

    Time: 0 ms (100%), Space: 46.3 MB (88.78%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -14,60 +14,31 @@ class Node {
 */
 
 class Solution {
-    public Node deepCopy(Node head){
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
+        //deep copy of the original list
         Node newHead = new Node(head.val);
-        Node temp = head.next;
         Node curr = newHead;
+        Node temp = head.next;
         while(temp != null){
             curr.next = new Node(temp.val);
             curr = curr.next;
             temp = temp.next;
         }
-        return newHead;
-    }
-    public void connectAlternatively(Node head, Node newHead){
-        Node dummy = new Node(-1);
-        Node temp = dummy;
-        Node temp1 = head;
-        Node temp2 = newHead;
-        while(temp1 != null && temp2 != null){
-            temp.next = temp1;
-            temp1 = temp1.next;
-            temp = temp.next;
-            temp.next = temp2;
-            temp2 = temp2.next;
-            temp = temp.next;
-        }
-    }
-    public void splitLists(Node head, Node newHead){
+        HashMap<Node, Node> map = new HashMap<>();
         Node t1 = head;
         Node t2 = newHead;
         while(t1 != null){
-            t1.next = t2.next;
+            map.put(t1, t2);
             t1 = t1.next;
-            if(t1 == null) break;
-            t2.next = t1.next;
             t2 = t2.next;
         }
-    }
-    public Node copyRandomList(Node head) {
-        //edge case handles empty list
-        if(head == null) return null;
-        // deep copying the given list
-        Node newHead = deepCopy(head);
-        // alternately making the connections with original and copied list
-        connectAlternatively(head, newHead);
-        //assigning random pointers
-        Node t1 = head;
-        Node t2 = newHead;
-        while(t1 != null){
-            t2 = t1.next;
-            if(t1.random != null) t2.random = t1.random.next;
-            t1 = t1.next.next;
+        for(Node original : map.keySet()){
+            Node duplicate = map.get(original);
+            if(original.random != null){
+                duplicate.random = map.get(original.random);
+            }
         }
-        //split the two lists
-        splitLists(head, newHead);
-        // return the head of new list
         return newHead;
     }
 }
